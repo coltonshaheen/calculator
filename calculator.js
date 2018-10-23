fba815e0faa6d6b956a5431df6d86c4f3b9e4f47
Colton Shaheen <coltonjshaheen@gmail.com>
2018-10-23 19:16:06 -0400

    JS now functions with DOM. Calculates one equation and displays.

--- calculator.js
+++ calculator.js
@@ -1,19 +1,29 @@
-let operandArray = [];
+const NUMBEROFKEYS = 9;
+const NUMBEROFDIGITS = 9;
+
+const operatorDisplay = document.getElementById('operator');
+const operandDisplay = document.getElementById('operand');
+const resultDisplay = document.getElementById('result');
+
+let firstOperand = 0;
+let secondOperand = 0;
+let previousOperator = '';
+let result = 0;
 
 function add(num1, num2) {
-    return num1 + num2;
+	return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+	return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+	return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+	return (num2 === 0 && num1 !== 0) ? "UNDEFINED" : (num1 / num2);
 }
 
 function operate(num1, num2, operator) {
@@ -28,27 +38,70 @@ function operate(num1, num2, operator) {
 	}
 }
 
-function makeOperandArray(number) {
-	operandArray.push(number);
-	console.log(operandArray);
+function initOperation(operator) {
+	previousOperator = operator;
+
+	firstOperand = Number(operandDisplay.textContent);
+	operandDisplay.textContent = '';
+	
+	if (operator === 'add') {
+		operatorDisplay.textContent = '+';
+	} else if (operator === 'subtract') {
+		operatorDisplay.textContent = '-';
+	} else if (operator === 'multiply') {
+		operatorDisplay.textContent = 'x';
+	} else if (operator === 'divide') {
+		operatorDisplay.textContent = '÷';
+	}
 }
 
-document.getElementById('1').addEventListener('click', makeOperandArray(1));
-const button2 = document.getElementById('2').addEventListener('click', makeOperandArray(2));
-const button3 = document.getElementById('3').addEventListener('click', makeOperandArray(3));
-const button4 = document.getElementById('4').addEventListener('click', makeOperandArray(4));
-const button5 = document.getElementById('5').addEventListener('click', makeOperandArray(5));
-const button6 = document.getElementById('6').addEventListener('click', makeOperandArray(6));
-const button7 = document.getElementById('7').addEventListener('click', makeOperandArray(7));
-const button8 = document.getElementById('8').addEventListener('click', makeOperandArray(8));
-const button9 = document.getElementById('9').addEventListener('click', makeOperandArray(9));
-const button0 = document.getElementById('0').addEventListener('click', makeOperandArray(0));
-const buttonPeriod = document.getElementById('period').addEventListener('click', makeOperandArray('.'));
+function displayOperand(number) {
+	operandText = operandDisplay.textContent.length;
+	if (operandText < NUMBEROFDIGITS) {
+	operandDisplay.textContent += number;
+	} return;
+}
 
-const buttonPlus = document.getElementById('plus');
-const buttonMinus = document.getElementById('minus');
-const buttonTimes = document.getElementById('times');
-const buttonDivided = document.getElementById('divided');
+function compute() {
+	secondOperand = operandDisplay.textContent;
+	
+	let result = operate(firstOperand, secondOperand, previousOperator);
+	
+	operandDisplay.textContent = result;
+}
 
-const buttonEquals = document.getElementById('');const button = document.getElementById('equals');
+window.onload = function addNumberListeners() {
+	for (let i = NUMBEROFKEYS; i >= 0; i--) {
+		let numString = i + '';
+
+		document.getElementById(numString).addEventListener('click', function () {
+			displayOperand(i);
+		});
+	}
+
+	document.getElementById('period').addEventListener('click', function () {
+		if (operandDisplay.textContent.includes('.')) {
+			return;
+		} displayOperand('.');
+	});
+}
+
+const buttonPlus = document.getElementById('plus').addEventListener('click', function() {
+	initOperation('add');
+});
+
+const buttonMinus = document.getElementById('minus').addEventListener('click', function() {
+	initOperation('subtract');
+});
+const buttonTimes = document.getElementById('times').addEventListener('click', function() {
+	initOperation('multiply');
+});
+const buttonDivided = document.getElementById('divided').addEventListener('click', function() {
+	initOperation('divide');
+});
+
+const buttonEquals = document.getElementById('equals').addEventListener('click', function() {
+	compute();
+});
+
 const buttonClear = document.getElementById('clear');
